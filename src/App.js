5e4fb59fca77c0e9250020ce8a98d29d84aed953
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-11-21 17:52:10 -0500

    Styling

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App flex flex-wrap justify-center">
       <Header />
       <div className="flex flex-wrap text-center p-4 bg-white">
-        <h3 className="capitalize text-gray-800 w-full mx-auto py-2 px-4 text-2xl lg:text-3xl font-bold">Not sure what size <FontAwesomeIcon icon={faBox} className="mx-1 text-green-600" /> you need?</h3>
+        <h3 className="capitalize text-gray-800 w-full mx-auto py-2 px-4 text-2xl lg:text-4xl font-bold mb-2">Not sure what size <FontAwesomeIcon icon={faBox} className="mx-1 text-green-600" /> you need?</h3>
         <p className="text-gray-600 w-full mb-8 text-lg lg:text-xl font-semibold">Use our handy calculator below and we will recommend one of our products for you!</p>
         <button
           className="text-lg md:text-2xl bg-green-500 rounded-lg border-b-4 border-green-600 focus:border-t-4 focus:border-b-0 focus:shadow-inner mx-auto p-4 font-bold text-white hover:bg-green-400"
@@ -33,12 +33,12 @@ function App() {
       )}
 
       <div className="flex w-full md:w-1/2">
-        {productName && <div className="text-gray-700 w-full text-2xl my-10 font-bold text-center tracking-wide">
+        {productName && <div className="text-gray-700 w-full text-2xl mt-4 mb-10 font-bold text-center tracking-wide">
           <span className="text-green-600">We Recommend:</span> <span className="underline decoration-wavy decoration-green-500 underline-offset-8">{productName}</span>
         </div>}
       </div>
 
-      <div id="footer" className="bg-gray-200 w-full absolute bottom-0 p-8 text-sm text-gray-500 text-right">
+      <div id="footer" className="bg-gray-200 w-full absolute bottom-0 p-4 md:p-8 text-sm text-gray-500 text-center md:text-right">
         <p>Copyright &copy; 2023 Ship Sticks. All Rights Reserved.</p>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,9 @@ function Header() {
 			<div className="w-full md:w-4/6 flex mx-auto justify-center md:justify-start mb-10">
 				<img src={logo} alt="Ship Sticks Company Logo" width="150" />
 			</div>
-			<h2 className="text-4xl lg:text-7xl font-bold mb-4 bg-gradient-to-b from-green-200 via-green-400 to-green-600 bg-clip-text text-transparent">Ship Your Golf Clubs Ahead</h2>
+			<h2 className="text-4xl lg:text-7xl font-bold mb-4 bg-gradient-to-b from-green-200 via-green-400 to-green-600 bg-clip-text text-transparent">
+				Ship Your Golf Clubs Ahead
+			</h2>
 			<h3 className="text-xl lg:text-3xl font-semibold text-shadow-lg shadow-green-500">
 				Safe, convenient, and hassle-free travel. Guaranteed.
 			</h3>
