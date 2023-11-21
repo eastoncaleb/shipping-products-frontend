140f7775709519e982c64589d06a659ab5128098
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-11-21 18:36:05 -0500

    Add additional padding to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 function Header() {
 	return (
-		<div id="header" className="p-16 mb-4 drop-shadow-xl text-center text-white w-full">
+		<div id="header" className="p-20 mb-4 drop-shadow-xl text-center text-white w-full">
 			<h2 className="text-4xl lg:text-7xl font-bold mb-4 bg-gradient-to-b from-green-200 via-green-400 to-green-600 bg-clip-text text-transparent">
 				Ship Your Golf Clubs Ahead
 			</h2>
