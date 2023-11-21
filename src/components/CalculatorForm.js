605ab682512d9900c70d764db5abeb2b726e46d6
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-11-21 18:29:01 -0500

    Add basic form validation

--- src/components/CalculatorForm.js
+++ src/components/CalculatorForm.js
@@ -5,46 +5,59 @@ function CalculatorForm({ onSubmit }) {
 	const [width, setWidth] = useState('');
 	const [height, setHeight] = useState('');
 	const [weight, setWeight] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		onSubmit({ length, width, height, weight });
+		if (length && width && height && weight) {
+			onSubmit({ length, width, height, weight });
+		} else {
+			setErrorMessage('Please fill in all form fields');
+		}
 	};
 
 	return (
-		<form onSubmit={handleSubmit} className="flex flex-wrap lg:flex-no-wrap w-full">
-			<div className="rounded-lg divide-y lg:divide-y-0 lg:divide-x">
-				<input
-					type="number"
-					placeholder="Length"
-					className="w-full lg:w-auto p-6 rounded-t-lg lg:rounded-tr-none lg:rounded-l-lg"
-					value={length}
-					onChange={(e) => setLength(e.target.value)}
-				/>
-				<input
-					type="number"
-					placeholder="Width"
-					className="w-full lg:w-auto p-6"
-					value={width}
-					onChange={(e) => setWidth(e.target.value)}
-				/>
-				<input
-					type="number"
-					placeholder="Height"
-					className="w-full lg:w-auto p-6"
-					value={height}
-					onChange={(e) => setHeight(e.target.value)}
-				/>
-				<input
-					type="number"
-					placeholder="Weight"
-					className="w-full lg:w-auto p-6 rounded-b-lg lg:rounded-bl-none lg:rounded-r-lg"
-					value={weight}
-					onChange={(e) => setWeight(e.target.value)}
-				/>
-			</div>
-			<button type="submit" className="w-full my-4 lg:my-0 lg:w-auto bg-green-500 rounded-lg border-b-4 border-green-600 focus:border-t-4 focus:border-b-0 focus:shadow-inner mx-0 lg:mx-2 p-4 font-bold text-white hover:bg-green-400">Find Product</button>
-		</form>
+		<>
+			{errorMessage && <p className="bg-red-500/50 font-bold text-red-100 mb-2 rounded-lg p-4 text-center">{errorMessage}</p>}
+			<form onSubmit={handleSubmit} className="flex flex-wrap lg:flex-no-wrap w-full">
+				<div className="rounded-lg divide-y lg:divide-y-0 lg:divide-x">
+					<input
+						type="number"
+						placeholder="Length"
+						className="w-full lg:w-auto p-6 rounded-t-lg lg:rounded-tr-none lg:rounded-l-lg"
+						value={length}
+						onChange={(e) => setLength(e.target.value)}
+					/>
+					<input
+						type="number"
+						placeholder="Width"
+						className="w-full lg:w-auto p-6"
+						value={width}
+						onChange={(e) => setWidth(e.target.value)}
+					/>
+					<input
+						type="number"
+						placeholder="Height"
+						className="w-full lg:w-auto p-6"
+						value={height}
+						onChange={(e) => setHeight(e.target.value)}
+					/>
+					<input
+						type="number"
+						placeholder="Weight"
+						className="w-full lg:w-auto p-6 rounded-b-lg lg:rounded-bl-none lg:rounded-r-lg"
+						value={weight}
+						onChange={(e) => setWeight(e.target.value)}
+					/>
+				</div>
+				<button
+					type="submit"
+					className="w-full my-4 lg:my-0 lg:w-auto bg-green-500 rounded-lg border-b-4 border-green-600 focus:border-t-4 focus:border-b-0 focus:shadow-inner mx-0 lg:ml-2 p-4 font-bold text-white hover:bg-green-400">
+					Find Product
+				</button>
+
+			</form>
+		</>
 	);
 }
 
