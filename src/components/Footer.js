813a301c2d92984c0541bcd6d69320129b5ad7bb
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-11-21 18:35:34 -0500

    Remove company branding because not sure if that is allowed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
 	return (
 		<div id="footer" className="bg-gray-200 w-full absolute bottom-0 p-4 md:p-8 text-sm text-gray-500 text-center md:text-right">
-			<p>Copyright &copy; 2023 Ship Sticks. All Rights Reserved.</p>
+			<p>Copyright &copy; 2023. All Rights Reserved.</p>
 		</div>
 	)
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,6 @@
-import logo from '../assets/company-logo.png';
-
 function Header() {
 	return (
-		<div id="header" className="p-8 mb-4 drop-shadow-xl text-center text-white w-full">
-			<div className="w-full md:w-4/6 flex mx-auto justify-center md:justify-start mb-10">
-				<img src={logo} alt="Ship Sticks Company Logo" width="150" />
-			</div>
+		<div id="header" className="p-16 mb-4 drop-shadow-xl text-center text-white w-full">
 			<h2 className="text-4xl lg:text-7xl font-bold mb-4 bg-gradient-to-b from-green-200 via-green-400 to-green-600 bg-clip-text text-transparent">
 				Ship Your Golf Clubs Ahead
 			</h2>
